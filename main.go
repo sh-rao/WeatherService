a1d0ff73f236c5bfa05c3c7d54567c650b43bc59
Shankar Rao <Shankar.Rao@latitudefinancial.com>
2020-04-02 23:16:50 +1100

    Panic if we can't load the config

--- main.go
+++ main.go
@@ -27,11 +27,13 @@ func initConfig() *cfg.Configuration {
 
 	if err := viper.ReadInConfig(); err != nil {
 		fmt.Printf("Error reading config file, %s", err)
+		panic(err)
 	}
 
 	err := viper.Unmarshal(&configuration)
 	if err != nil {
 		fmt.Printf("Unable to decode into struct, %v", err)
+		panic(err)
 	}
 
 	return &configuration
